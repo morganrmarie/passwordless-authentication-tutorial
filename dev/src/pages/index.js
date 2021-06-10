e666e8891fb02cf50809312ac9aa101edd4fdfbe
Morgan Richardson <morganrmarie@gmail.com>
2021-06-09 22:47:30 -0500

    Prep dev for tutorial

--- dev/src/pages/index.js
+++ dev/src/pages/index.js
@@ -1,52 +1,84 @@
-import * as React from "react"
+import React, { useEffect, useState, createRef } from "react"
 import Grid from "@material-ui/core/Grid"
 import Avatar from "../images/avatar.png"
 import "../styles/index.scss"
+import { UserContext } from "../lib/UserContext"
+import Cursor from "../images/cursor.png"
 
 const IndexPage = () => {
+  const [user, setUser] = useState(null)
+  let userEmail = createRef()
+
+  const handleEmailLogin = async (e) => {}
+
+  const handleLogout = async () => {}
+
+  useEffect(() => {}, [])
+
   return (
-    <div className="Index__Wrapper">
-      <title>Magic Tutorial: Passwordless Authentication</title>
-      <Grid container className="Header">
-        <Grid item xs={12}>
-          <a href="https://medium.com" target="_blank" rel="noreferrer">
-            Magic + Gatsby.js Tutorial
-          </a>
+    <UserContext.Provider value={[user, setUser]}>
+      <div className="IndexWrapper">
+        <title>Magic Tutorial: Passwordless Authentication</title>
+        <Grid container className="Header">
+          <Grid item xs={12}>
+            <a href="https://medium.com" target="_blank" rel="noreferrer">
+              Magic + Gatsby.js Tutorial
+            </a>
+          </Grid>
+          <Grid item xs={12}>
+            <a
+              href="https://github.com/morganrmarie/passwordless-authentication-tutorial"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Full Source Code
+            </a>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <a
-            href="https://github.com/morganrmarie/passwordless-authentication-tutorial"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Full Source Code
-          </a>
+        <Grid container className="IndexContainer">
+          <Grid item xs={12}>
+            <div className="LoginContainer">
+              {user ? <h1>Logged in!</h1> : <h1>Login</h1>}{" "}
+              <img src={Cursor} alt="Cursor" />
+            </div>
+            {user ? (
+              <div className="LoggedInInfo">
+                <p>
+                  Email: <span>{user.email}</span>
+                </p>
+                <p>
+                  Public address: <span>{user.publicAddress}</span>
+                </p>
+              </div>
+            ) : (
+              <input
+                type="text"
+                placeholder="Enter your email"
+                autoCorrect="off"
+                ref={userEmail}
+              />
+            )}
+            <button onClick={user ? handleLogout : handleEmailLogin}>
+              <p>{user ? "Logout" : "Send Magic Link"}</p>
+            </button>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container className="Index__Container">
-        <Grid item xs={12}>
-          <h1>Login</h1>
-          <input type="text" placeholder="Enter your email" autocorrect="off" />
-          <button>
-            <p>Send Magic Link</p>
-          </button>
+        <Grid container className="Footer">
+          <Grid item xs={2}>
+            <img src={Avatar} alt="Avatar" />
+          </Grid>
+          <Grid item xs={10} className="FooterLink">
+            <a
+              href="https://instagram.com/morgan.codes"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Created by MorganCodes</p>
+            </a>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container className="Footer">
-        <Grid item xs={2}>
-          <img src={Avatar} alt="Avatar" />
-        </Grid>
-        <Grid item xs={10} className="Footer__Link">
-          <a
-            href="https://instagram.com/morgan.codes"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p>Created by MorganCodes</p>
-          </a>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </UserContext.Provider>
   )
 }
 
